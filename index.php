9d17eca166432d987d47d65bb1c98268ed0b995c
Ridx <daniel.swim95@googlemail.com>
2014-12-24 15:04:46 +0100

    0.1.6

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+
+
+<head lang="de">
+	<meta name="author" content="Daniel Ochs">
+    <meta charset="UTF-8">
+    <title>iRevYou</title>
+    <meta name="description" content="IRevYou">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+		
+    <!--adds HTML5 and mediaqueries-->   
+	<!--[if lt IE 9]>
+	<script src="js/html5shiv.js"></script> 
+    <script src="js/respond.js"></script>
+	<![endif]-->
+	
+<!--<script src="modernizr.custom.14167.js"></script> -->
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:700,300,400' rel='stylesheet' type='text/css'><!--OpenSansFont 700 300 400 -->
+	<link rel="stylesheet" type="text/css" media="screen" href="style.css">
+	<link rel="stylesheet" type="text/css" media="screen" href="normalize.css">
+</head>
+
+<body>
+
+
+<header class="columns">
+
+<!-- Logo -->
+<img src="resources/logo_v3.png"  alt="logo" class="logo">
+
+<!-- Slogan -->
+<!-- <br>beauty and lifestyle -->
+
+<!-- Registerbox  -->
+<br>
+<!-- <div class="register "> -->
+			<form action="newsletter.php" methods="get" >
+				<input type="email" name="address"  placeholder="E-Mail"  class="" autocomplete="off" autofocus/> 
+				<br><input type="submit" name="register"  alt="register" class="" value="Registrieren"/>        
+			</form>
+
+
+<!--</div> -->
+</header>
+
+
+<div class="content  columns">
+
+
+<div class="element ele-1" >
+<img src="resources/youtube.png" width="75px" height="75px" alt="YouTube-Icon" class="icon">
+<strong> Dein Youtube-Kanal</strong><br>
+Du möchtest kostenlos<br>
+neue Produkte in <br> 
+deinen Videos testen?<br>
+<br>
+Dann registriere dich jetzt!<br>
+</div>
+
+
+<div class="element ele-2 ">
+<img src="resources/irevyou.png" width="75px" height="75px" alt="IRevYou-Icon" class="icon">
+<strong> Wir über Uns</strong><br>
+iRevYou ist eine Plattform,<br>
+die Youtuber und Unternehmen<br>
+in Kontakt bringt.<br>
+<br>
+Einfach und unkompliziert.<br>
+</div>
+
+
+<div class="element ele-3 ">
+<img src="resources/company.png" width="75px" height="75px" alt="Company-Icon" class="icon" >
+<strong> Ihr Unternehmen</strong><br>
+Sie möchten Ihr Produkt <br>
+ohne viel Aufwand über <br>
+Youtube-Kanäle promoten? <br>
+<br>
+Wir haben die Lösung.<br>
+</div>
+
+
+</div>
+
+<footer> 
+<a href="impressum.php">Impressum<a>
+</footer>
+
+
+
+
+
+</body>
+</html>
